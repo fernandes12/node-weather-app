58fff19710f415223705f99d25a4ee31b7dda8f2
fernandes12 <fernandesjhd@gmail.com>
2021-04-15 11:06:12 +0100

    forecaste update 3

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,6 @@ const forecast = (latitude, longitude, callback) => {
         }else if(body.error){
                 callback('Unable to find location!', undefined)
             } else {
-                console.log(body.current.feelslike)
                 callback(undefined,body.current.weather_descriptions[0]+ '. It is currently ' +body.current.temperature+' degrees out, and it feels like '+
                 body.current.feelslike+ " degrees out. The humidity is " +body.current.humidity+ "%.")
             }
